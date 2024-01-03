bf921aee38bc869d48c59b6389b3e5ae4fbbe20f
Søren Larsen Jelsdal <slj@jjkommunikation.dk>
2024-01-03 11:39:47 +0100

    styling feature next prev

--- wp-content/themes/jj-kommunikation/jj-kommunikation/js/customizer.js
+++ wp-content/themes/jj-kommunikation/jj-kommunikation/js/customizer.js
@@ -18,6 +18,7 @@ jQuery(document).ready(function(){
 
 const menuItems = Array.from(document.querySelectorAll("#ribbon-section ul.menu li a"));
 const currentUrl = window.location.href;
+console.log(currentUrl + ' nuværende url');
 
 const nextButton = document.createElement("a");
 nextButton.id = "next-button";
@@ -26,6 +27,8 @@ const prevButton = document.createElement("a");
 prevButton.id = "prev-button";
 
 const ribbonSection = document.querySelector("#ribbon-section");
+
+
 
 ribbonSection.appendChild(prevButton);
 ribbonSection.appendChild(nextButton);
@@ -36,20 +39,28 @@ function updateButtonsText() {
 
 
  menuItems.forEach(function(menuItem, index) {
+	console.log(menuItem.attributes[0]);
 
-	if (menuItem.attributes[0].nodeValue == currentUrl) {
+	if (currentUrl.includes(menuItem.attributes[0].nodeValue)) {
+		const lastItem = menuItems.length - 1;
+	console.log(menuItems);
+		if (index === lastItem) {
+		nextButton.innerHTML
+		nextButton.innerHTML = `<span class='feature-nav next'>Næste feature</span> <img src='${menuItems[0].firstElementChild.attributes[0].nodeValue}'><span class='feature-name'>${menuItems[0].firstChild.data}</span>`;
+		nextButton.setAttribute("href", menuItems[0]);
+		} else {
+			nextButton.innerHTML = `<span class='feature-nav next'>Næste feature</span> <img src='${menuItems[index + 1].firstElementChild.attributes[0].nodeValue}'><span class='feature-name'>${menuItems[index + 1].firstChild.data}</span>`;
+			nextButton.setAttribute("href", menuItems[index + 1]);
+		}
 		
-	
-		nextButton.textContent = `Next: ${menuItems[index + 1]}`;
-		nextButton.setAttribute("href", menuItems[index + 1]);
 		if (index == 0) {
-			const lastItem = menuItems.length - 1;
-			prevButton.textContent = `Prev: ${menuItems[index + lastItem]}`;
+			
+			prevButton.innerHTML = `<span class='feature-nav next'>Forrige feature</span> <img src='${menuItems[index + lastItem].firstElementChild.attributes[0].nodeValue}'><span class='feature-name'>${menuItems[index + lastItem].firstChild.data}</span>`;
 			prevButton.setAttribute("href", menuItems[index + lastItem]);
 
 		} else {
-			prevButton.textContent = `Prev: ${menuItems[index + 1]}`;
-			prevButton.setAttribute("href", menuItems[index + 1]);
+			prevButton.innerHTML = `<span class='feature-nav next'>Forrige feature</span> <img src='${menuItems[index - 1].firstElementChild.attributes[0].nodeValue}'><span class='feature-name'>${menuItems[index - 1].firstChild.data}</span>`;
+			prevButton.setAttribute("href", menuItems[index - 1]);
 		}
 
 
