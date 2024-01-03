bf759fe4f9aa92c17a8b05ca82d579251b69b0b0
Søren Larsen Jelsdal <slj@jjkommunikation.dk>
2024-01-03 15:52:01 +0100

    styling feature next prev

--- wp-content/themes/jj-kommunikation/jj-kommunikation/js/customizer.js
+++ wp-content/themes/jj-kommunikation/jj-kommunikation/js/customizer.js
@@ -18,7 +18,6 @@ jQuery(document).ready(function(){
 
 const menuItems = Array.from(document.querySelectorAll("#ribbon-section ul.menu li a"));
 const currentUrl = window.location.href;
-console.log(currentUrl + ' nuværende url');
 
 const nextButton = document.createElement("a");
 nextButton.id = "next-button";
@@ -39,27 +38,94 @@ function updateButtonsText() {
 
 
  menuItems.forEach(function(menuItem, index) {
-	console.log(menuItem.attributes[0]);
+	//console.log(menuItem.attributes[0]);
 
 	if (currentUrl.includes(menuItem.attributes[0].nodeValue)) {
 		const lastItem = menuItems.length - 1;
-	console.log(menuItems);
+	//console.log(menuItems);
 		if (index === lastItem) {
-		nextButton.innerHTML
-		nextButton.innerHTML = `<span class='feature-nav next'>Næste feature</span> <img src='${menuItems[0].firstElementChild.attributes[0].nodeValue}'><span class='feature-name'>${menuItems[0].firstChild.data}</span>`;
+			let strNext = menuItems[0].children[2].attributes[1].nodeValue;
+			let color = strNext.match(/#([0-9a-fA-F]{3,6})/)[0];
+			nextButton.innerHTML = `<svg width="97" height="182" viewBox="0 0 97 182" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<g id="Nextbutton - Planl&#195;&#166;gning">
+			<path id="Vector" d="M103.942 7.94709e-06C157.18 1.25779e-05 188 13.1843 188 80.5856L188 101.414C188 168.816 157.18 182 103.942 182L84.0583 182C30.8203 182 1.64172e-05 168.816 2.23394e-05 101.414L2.41695e-05 80.5856C3.00916e-05 13.1842 30.8203 1.58673e-06 84.0584 6.21757e-06L103.942 7.94709e-06Z" fill="${color}"/>
+			</g>
+			<defs>
+			<linearGradient id="paint0_linear_701_926" x1="-81.051" y1="-10.6611" x2="183.67" y2="144.537" gradientUnits="userSpaceOnUse">
+			<stop stop-color="#E2BC5D"/>
+			<stop offset="1" stop-color="#E2BC5D"/>
+			</linearGradient>
+			</defs>
+			</svg><span class='feature-nav next'>Næste feature
+			<svg width="27" height="15" viewBox="0 0 27 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path id="Vector 26" d="M0.937988 2.62136C6.77132 -0.211977 19.538 -1.97864 23.938 13.6214M23.938 13.6214L18.938 12.6211M23.938 13.6214L25.938 9.12109" stroke="white"/>
+			</svg>
+			</span> <img src='${menuItems[0].firstElementChild.attributes[0].nodeValue}'><span class='feature-name'>${menuItems[0].firstChild.data}</span>`;
 		nextButton.setAttribute("href", menuItems[0]);
 		} else {
-			nextButton.innerHTML = `<span class='feature-nav next'>Næste feature</span> <img src='${menuItems[index + 1].firstElementChild.attributes[0].nodeValue}'><span class='feature-name'>${menuItems[index + 1].firstChild.data}</span>`;
+			//nextButton.setAttribute("style", menuItems[index + 1].children[2].attributes[1].nodeValue);
+			let strNext = menuItems[index + 1].children[2].attributes[1].nodeValue;
+			let color = strNext.match(/#([0-9a-fA-F]{3,6})/)[0];
+			nextButton.innerHTML = `<svg width="97" height="182" viewBox="0 0 97 182" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<g id="Nextbutton - Planl&#195;&#166;gning">
+			<path id="Vector" d="M103.942 7.94709e-06C157.18 1.25779e-05 188 13.1843 188 80.5856L188 101.414C188 168.816 157.18 182 103.942 182L84.0583 182C30.8203 182 1.64172e-05 168.816 2.23394e-05 101.414L2.41695e-05 80.5856C3.00916e-05 13.1842 30.8203 1.58673e-06 84.0584 6.21757e-06L103.942 7.94709e-06Z" fill="${color}"/>
+			</g>
+			<defs>
+			<linearGradient id="paint0_linear_701_926" x1="-81.051" y1="-10.6611" x2="183.67" y2="144.537" gradientUnits="userSpaceOnUse">
+			<stop stop-color="#E2BC5D"/>
+			<stop offset="1" stop-color="#E2BC5D"/>
+			</linearGradient>
+			</defs>
+			</svg><span class='feature-nav next'>Næste feature
+			<svg width="27" height="15" viewBox="0 0 27 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path id="Vector 26" d="M0.937988 2.62136C6.77132 -0.211977 19.538 -1.97864 23.938 13.6214M23.938 13.6214L18.938 12.6211M23.938 13.6214L25.938 9.12109" stroke="white"/>
+			</svg>
+			</span> <img src='${menuItems[index + 1].firstElementChild.attributes[0].nodeValue}'><span class='feature-name'>${menuItems[index + 1].firstChild.data}</span>`;
 			nextButton.setAttribute("href", menuItems[index + 1]);
 		}
 		
 		if (index == 0) {
-			
-			prevButton.innerHTML = `<span class='feature-nav next'>Forrige feature</span> <img src='${menuItems[index + lastItem].firstElementChild.attributes[0].nodeValue}'><span class='feature-name'>${menuItems[index + lastItem].firstChild.data}</span>`;
-			prevButton.setAttribute("href", menuItems[index + lastItem]);
+			let str = menuItems[lastItem].children[2].attributes[1].nodeValue;
+			let color = str.match(/#([0-9a-fA-F]{3,6})/)[0];
+			prevButton.innerHTML = `<svg width="97" height="182" viewBox="0 0 97 182" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<g id="Nextbutton - Planl&#195;&#166;gning">
+			<path id="Vector" d="M103.942 7.94709e-06C157.18 1.25779e-05 188 13.1843 188 80.5856L188 101.414C188 168.816 157.18 182 103.942 182L84.0583 182C30.8203 182 1.64172e-05 168.816 2.23394e-05 101.414L2.41695e-05 80.5856C3.00916e-05 13.1842 30.8203 1.58673e-06 84.0584 6.21757e-06L103.942 7.94709e-06Z" fill="${color}"/>
+			</g>
+			<defs>
+			<linearGradient id="paint0_linear_701_926" x1="-81.051" y1="-10.6611" x2="183.67" y2="144.537" gradientUnits="userSpaceOnUse">
+			<stop stop-color="#E2BC5D"/>
+			<stop offset="1" stop-color="#E2BC5D"/>
+			</linearGradient>
+			</defs>
+			</svg>			
+			<span class='feature-nav prev'>Forrige feature
+			<svg width="27" height="15" viewBox="0 0 27 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path id="Vector 26" d="M0.937988 2.62136C6.77132 -0.211977 19.538 -1.97864 23.938 13.6214M23.938 13.6214L18.938 12.6211M23.938 13.6214L25.938 9.12109" stroke="white"/>
+			</svg>
+			</span> <img src='${menuItems[lastItem].firstElementChild.attributes[0].nodeValue}'><span class='feature-name'>${menuItems[index + lastItem].firstChild.data}</span>`;
+			prevButton.setAttribute("href", menuItems[lastItem]);
 
 		} else {
-			prevButton.innerHTML = `<span class='feature-nav next'>Forrige feature</span> <img src='${menuItems[index - 1].firstElementChild.attributes[0].nodeValue}'><span class='feature-name'>${menuItems[index - 1].firstChild.data}</span>`;
+			
+			//prevButton.setAttribute("style", menuItems[index - 1].children[2].attributes[1].nodeValue);
+			let str = menuItems[index - 1].children[2].attributes[1].nodeValue;
+			let color = str.match(/#([0-9a-fA-F]{3,6})/)[0];
+			prevButton.innerHTML = `<svg width="97" height="182" viewBox="0 0 97 182" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<g id="Nextbutton - Planl&#195;&#166;gning">
+			<path id="Vector" d="M103.942 7.94709e-06C157.18 1.25779e-05 188 13.1843 188 80.5856L188 101.414C188 168.816 157.18 182 103.942 182L84.0583 182C30.8203 182 1.64172e-05 168.816 2.23394e-05 101.414L2.41695e-05 80.5856C3.00916e-05 13.1842 30.8203 1.58673e-06 84.0584 6.21757e-06L103.942 7.94709e-06Z" fill="${color}"/>
+			</g>
+			<defs>
+			<linearGradient id="paint0_linear_701_926" x1="-81.051" y1="-10.6611" x2="183.67" y2="144.537" gradientUnits="userSpaceOnUse">
+			<stop stop-color="#E2BC5D"/>
+			<stop offset="1" stop-color="#E2BC5D"/>
+			</linearGradient>
+			</defs>
+			</svg>			
+			<span class='feature-nav prev'>Forrige feature
+			<svg width="27" height="15" viewBox="0 0 27 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path id="Vector 26" d="M0.937988 2.62136C6.77132 -0.211977 19.538 -1.97864 23.938 13.6214M23.938 13.6214L18.938 12.6211M23.938 13.6214L25.938 9.12109" stroke="white"/>
+			</svg>
+</span> <img src='${menuItems[index - 1].firstElementChild.attributes[0].nodeValue}'><span class='feature-name'>${menuItems[index - 1].firstChild.data}</span>`;
 			prevButton.setAttribute("href", menuItems[index - 1]);
 		}
 
@@ -75,3 +141,31 @@ function updateButtonsText() {
 }
 
 updateButtonsText();
+
+/*
+document.addEventListener("DOMContentLoaded", function() {
+	// Check if the 'visited' flag is set in local storage
+	if (!localStorage.getItem('visited')) {
+	  // If not visited, add class to the element
+	  document.getElementById('ribbon-section').classList.add('incoming_tooltip');
+  
+	  // Set the 'visited' flag in local storage
+	  localStorage.setItem('visited', 'true');
+	}
+  }); */
+
+  document.addEventListener("DOMContentLoaded", function() {
+	// Check if the 'visited' flag is set in local storage
+	if (!localStorage.getItem('visited')) {
+	  // Add class to the element
+	  document.getElementById('ribbon-section').classList.add('incoming_tooltip');
+  
+	  // Add a mouseover event listener to set the 'visited' flag
+	  document.getElementById('ribbon-section').addEventListener('mouseover', function() {
+		localStorage.setItem('visited', 'true');
+		// Optional: Remove the class upon mouseover, if desired
+		this.classList.remove('incoming_tooltip');
+	  });
+	}
+  });
+  
