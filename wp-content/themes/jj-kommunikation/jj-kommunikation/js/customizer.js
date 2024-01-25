00b9bc879a5441117c224bf772000ee0cb8e7603
Søren Larsen Jelsdal <slj@jjkommunikation.dk>
2024-01-25 16:43:48 +0100

    styling

--- wp-content/themes/jj-kommunikation/jj-kommunikation/js/customizer.js
+++ wp-content/themes/jj-kommunikation/jj-kommunikation/js/customizer.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function(){
-
+/*
 		jQuery("li.menu-item-has-children").prepend("<button class='toggle_submenu'>+</button>");
   jQuery("button.toggle_submenu").click(function(){
 	  jQuery(this).siblings("ul.sub-menu").toggle(200,"linear");
 	  jQuery(this).text(jQuery(this).text() == '+' ? '-' : '+');
-	});
+	});*/
 	
 	jQuery(function() {
 		function adjustHeader() {
