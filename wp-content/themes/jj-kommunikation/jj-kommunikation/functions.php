8b13a5e3c9a84104fb570ce8cfc72ab6c93435eb
Leo Knudsen <info@jjkommunikation.dk>
2024-03-13 13:36:24 +0100

    reverting back from last change on functions.php

--- wp-content/themes/jj-kommunikation/jj-kommunikation/functions.php
+++ wp-content/themes/jj-kommunikation/jj-kommunikation/functions.php
@@ -378,6 +378,7 @@ function my_wp_nav_menu_objects( $items, $args ) {
 
 
 
+
 function mytheme_setup_theme_supported_features() {
     add_theme_support( 'editor-color-palette', array(
 		array(
@@ -466,13 +467,13 @@ function mytheme_setup_theme_supported_features() {
 			'color' => '#0D1D2C',
 		),
 		array(
-			'name' => ext_attr__( 'Engii Light blue', 'jj-kommunikation' ),
-			'slug' => 'Engii_Light_Blue',
-			'color' => '#F1F9FF'
-		)
+			'name'  => esc_attr__( 'Engii Footer', 'jj-kommunikation' ),
+			'slug'  => 'Blue_soft',
+			'color' => '#F1F9FF',
+		),
+		
     ) );
 }
-
 add_action( 'after_setup_theme', 'mytheme_setup_theme_supported_features' );
 
 
