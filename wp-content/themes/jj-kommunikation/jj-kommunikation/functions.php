2e9e3447fb8ea8660407ed3e569454e16a190b6a
Leo Knudsen <info@jjkommunikation.dk>
2024-03-13 13:42:12 +0100

    fixing functions.php to push again

--- wp-content/themes/jj-kommunikation/jj-kommunikation/functions.php
+++ wp-content/themes/jj-kommunikation/jj-kommunikation/functions.php
@@ -378,7 +378,6 @@ function my_wp_nav_menu_objects( $items, $args ) {
 
 
 
-
 function mytheme_setup_theme_supported_features() {
     add_theme_support( 'editor-color-palette', array(
 		array(
@@ -474,6 +473,7 @@ function mytheme_setup_theme_supported_features() {
 		
     ) );
 }
+
 add_action( 'after_setup_theme', 'mytheme_setup_theme_supported_features' );
 
 
