3dd4e834fe387ec1fd75d421dbb6bbf4c0d1ad65
Leo Knudsen <info@jjkommunikation.dk>
2024-04-04 15:29:17 +0200

    tryout changing sliding on 1024 breakpoint

--- wp-content/themes/jj-kommunikation/jj-kommunikation/js/swiper-custom.js
+++ wp-content/themes/jj-kommunikation/jj-kommunikation/js/swiper-custom.js
@@ -47,11 +47,6 @@ jQuery(document).ready(function(){
             },
 			1024: {
 				slidesPerView: 'auto',
-                spaceBetween: 30,
-                slideToClickedSlide: true,
-                roundLengths: true,
-                visibilityFullFit: true,
-                centeredSlies: true,
 			},
             1200: {
                 slidesPerView: 'auto',
