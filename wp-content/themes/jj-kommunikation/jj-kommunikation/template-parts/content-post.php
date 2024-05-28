5ea2b3366c45ed5f8f469140c80794eb8b3f75a0
Leo Knudsen <nightskater2200@gmail.com>
2024-05-28 15:32:16 +0200

    adding design changes to search results page

--- wp-content/themes/jj-kommunikation/jj-kommunikation/template-parts/content-post.php
+++ wp-content/themes/jj-kommunikation/jj-kommunikation/template-parts/content-post.php
@@ -10,28 +10,46 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
 	<div class="entry-content">
-	<?php //jj_kommunikation_post_thumbnail(); ?>
-		<div class="alignwide">
-			<div class="jj-content-archive">
-				<header class="entry-header">
-					<?php 
-						if (isset($_GET["s"])) {
-							echo "<a href='" . get_permalink() . "'><h2 class='entry-title'>" . get_the_title() . "</h2></a>";
-						} else {
-							echo the_title('<h2 class="entry-title">', '</h2>');
-						}
-					?>
-				</header><!-- .entry-header -->
-				<div class="jj-flex">
-					<div class="jj-content-blocks">
-						<?php
-							the_excerpt();
-						?>
-					</div><!-- .jj-content-blocks -->
-				</div><!-- .jj-flex -->
-			</div>
-		</div><!-- .alignwide -->
+		<?php //jj_kommunikation_post_thumbnail(); ?>
+		<?php
+			// render searchable results in a list if any is found
+			if ( isset($_GET["s"]) ) {
+				?>
+					<div class="alignwide">
+						<div class="jj-content-archive search-archive-wrapper">
+							<div class="search-item-entry">
+								<header class="entry-header">
+									<?php echo "<a href='" . get_permalink() . "'><h2 class='entry-title'>" . get_the_title() . "</h2></a>"; ?>
+								</header> <!-- .entry-header -->
+								<div class="jj-flex">
+									<div class="jj-content-blocks">
+										<div class="search-item-excerpt">
+											<?php the_excerpt(); ?>
+										</div>
+										<?php echo '<a class="search-item-seemore" href="' . get_permalink() . '">Se her</a>' ?>
+									</div> <!-- .jj-content-blocks -->
+								</div> <!-- .jj-flex -->
+							</div> <!-- .search-item-entry -->
+						</div> <!-- .jj-content-archive -->
+					</div> <!-- .alignwide -->
+				<?php
+			} else {
+				?>
+					<div class="alignwide">
+						<div class="jj-content-archive">
+							<header class="entry-header">
+								<?php echo the_title('<h2 class="entry-title">', '</h2>'); ?>
+							</header> <!-- .entry-header -->
+							<div class="jj-flex">
+								<div class="jj-content-blocks">
+									<?php the_excerpt(); ?>
+								</div> <!-- .jj-content-blocks -->
+							</div> <!-- .jj-flex -->
+						</div> <!-- .jj-content-archive -->
+					</div> <!-- .alignwide -->
+				<?php
+			}
+		?>
 	</div><!-- .entry-content -->
 </article><!-- #post-<?php the_ID(); ?> -->
